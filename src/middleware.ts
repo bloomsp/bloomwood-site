5902043b8ae524a36fbc1e8ed7e041ab72a0f1c8
Bloomy <steven@stevenbloom.com.au>
2026-04-05 12:19:35 +1000

    Add app CSP middleware

---
+++ src/middleware.ts
@@ -0,0 +1,63 @@
+import { defineMiddleware } from 'astro:middleware';
+
+const cspDirectives = {
+  'default-src': ["'self'"],
+  'base-uri': ["'self'"],
+  'form-action': ["'self'"],
+  'frame-ancestors': ["'self'"],
+  'object-src': ["'none'"],
+  'script-src': [
+    "'self'",
+    "'unsafe-inline'",
+    'https://challenges.cloudflare.com',
+    'https://cdn-cookieyes.com',
+    'https://embed.tawk.to',
+    'https://static.cloudflareinsights.com',
+    'https://cdn.jsdelivr.net',
+  ],
+  'style-src': ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
+  'font-src': ["'self'", 'data:', 'https://fonts.gstatic.com'],
+  'img-src': [
+    "'self'",
+    'data:',
+    'blob:',
+    'https://cdn-cookieyes.com',
+    'https://embed.tawk.to',
+    'https://*.tawk.to',
+    'https://va.tawk.to',
+  ],
+  'connect-src': [
+    "'self'",
+    'https://challenges.cloudflare.com',
+    'https://cdn-cookieyes.com',
+    'https://log.cookieyes.com',
+    'https://embed.tawk.to',
+    'https://va.tawk.to',
+    'https://cloudflareinsights.com',
+    'https://static.cloudflareinsights.com',
+    'wss://*.tawk.to',
+  ],
+  'frame-src': [
+    "'self'",
+    'https://challenges.cloudflare.com',
+    'https://embed.tawk.to',
+    'https://va.tawk.to',
+  ],
+  'worker-src': ["'self'", 'blob:'],
+  'media-src': ["'self'", 'blob:', 'https://*.tawk.to'],
+} as const;
+
+const contentSecurityPolicy = Object.entries(cspDirectives)
+  .map(([directive, values]) => `${directive} ${values.join(' ')}`)
+  .join('; ');
+
+export const onRequest = defineMiddleware(async (_, next) => {
+  const response = await next();
+  const contentType = response.headers.get('content-type') ?? '';
+
+  if (contentType.includes('text/html')) {
+    response.headers.set('Content-Security-Policy', contentSecurityPolicy);
+  }
+
+  return response;
+});
